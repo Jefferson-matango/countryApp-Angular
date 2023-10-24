1918a8b4b08a66fc816ad9f1edf6f8ddf46fbb6b
User <jbm.matango@yavirac.edu.ec>
2023-10-23 20:07:51 -0500

    First commit

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, of } from 'rxjs';
+
+import { Country } from '../interfaces/country';
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+
+  private apiUrl:string = 'https://restcountries.com/v3.1'
+
+  constructor(private http: HttpClient) { }
+
+  searchByCapital( term:string ):Observable<Country[]> {
+    const url = `${this.apiUrl}/capital/${term}`;
+    return this.http.get<Country[]>(url)
+    .pipe(
+      catchError(() => of ([]))
+    );
+  }
+
+  searchCountry(term: string):Observable<Country[]> {
+    return this.http.get<Country[]>(`${this.apiUrl}/name/${term}`)
+    .pipe(
+      catchError(()=>of([]))
+    )
+  }
+
+  searchRegion(region: string):Observable<Country[]> {
+    return this.http.get<Country[]>(`${this.apiUrl}/region/${region}`)
+    .pipe(
+      catchError(()=>of([]))
+    )
+  }
+}

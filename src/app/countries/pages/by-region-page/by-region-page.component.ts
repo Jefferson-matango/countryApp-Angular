9ec7b2bdf7434207a17af0414fe0934f24b74e32
User <jbm.matango@yavirac.edu.ec>
2023-10-30 17:44:13 -0500

    Proyecto finalizado

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { Country } from '../../interfaces/country';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
+
+import { Country } from '../../interfaces/country';
+import { Region } from '../../interfaces/region.type';
 
 @Component({
   selector: 'app-by-region-page',
@@ -8,13 +10,23 @@ import { CountriesService } from '../../services/countries.service';
   styles: [
   ]
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit{
 
   public countries: Country[] = [];
 
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe','Ocenia'];
+
+  public selectedRegion?: Region;
+
   constructor (private countriesService: CountriesService) {}
 
-  searchByRegion (region:string) {
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byRegion.countries;
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+  }
+
+  searchByRegion (region:Region) {
+    this.selectedRegion = region
     this.countriesService.searchRegion(region)
     .subscribe(countries=>
       this.countries=countries)
